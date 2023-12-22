4df218b1cc3e14b121cf4060127f292433a538d2
LAKGHA <82607992+LAKGHA@users.noreply.github.com>
2023-12-22 20:37:48 +0000

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "Anil-MaroGame" # Replace with your actual S3 bucket name
+    bucket = "Anil-MariGame" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
