6667db245e2c683091d7f95d52655fb4e8fb6b0c
LAKGHA <82607992+LAKGHA@users.noreply.github.com>
2023-12-22 20:41:00 +0000

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "anil-marioGame" # Replace with your actual S3 bucket name
+    bucket = "anil-mariogame" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
